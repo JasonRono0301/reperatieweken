455326707f348e6f2ff3819fd774d66a7e27c729
Jason <99057964@mydavinci.nl>
2021-03-23 14:43:30 +0100

    Console toegevoegd.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week2&3/Arrays/lusjes/forloop/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week2&3/Arrays/lusjes/forloop/script.js
@@ -3,3 +3,5 @@ var array = ['eten','drinken','pauze','auto'];//Array
 for(i=0; i < array.length; i++){//For loop.
     document.write(array[i] + "<br>");//Laat op het scherm zien.
 }
+
+console.log(array);
