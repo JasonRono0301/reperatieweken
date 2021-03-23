d498c3167f700aa988afca1fa8724b2ed66f5fb0
Jason <99057964@mydavinci.nl>
2021-03-23 12:46:04 +0100

    Oefeningen met array en loops.
    Heb geoefend met array en loops en het is gelukt.

---
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week2&3/Arrays/lusjes/foreach/script.js
@@ -0,0 +1,7 @@
+//For each.
+var array = ['Kaas','Paprika','Olijven'];
+array.forEach(loop);
+
+function loop(item, index){
+    document.write(item + " : " + index + "<br>");
+}
---
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week2&3/Arrays/lusjes/forloop/script.js
@@ -0,0 +1,5 @@
+//For loop array.
+var array = ['eten','drinken','pauze','auto'];//Array
+for(i=0; i < array.length; i++){//For loop.
+    document.write(array[i] + "<br>");//Laat op het scherm zien.
+}
