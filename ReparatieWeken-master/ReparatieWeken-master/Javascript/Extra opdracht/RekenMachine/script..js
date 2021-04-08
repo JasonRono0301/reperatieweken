eecb1d0d543950a551d982116a0f9f58122cb224
Jason <99057964@mydavinci.nl>
2021-04-08 13:09:11 +0200

    Heb een rekenmachine gemaakt.
    En het werkt.

---
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Extra opdracht/RekenMachine/script..js
@@ -0,0 +1,10 @@
+//Rekenmachine.
+
+ alert('Welkom bij de digitale rekenmachine.');
+
+ var getal1 =  Number( prompt('Voer een getal in'));
+ var getal2 = Number(prompt('Voer een getal in')); 
+
+ var uitkomst = getal1 + getal2;
+
+ alert('Het antwoord op jou som is ' + uitkomst);
