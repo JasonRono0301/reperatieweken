af8ba366293c37316dfc12b0395680a4f9f4a1c2
Jason <99057964@mydavinci.nl>
2021-04-13 14:20:59 +0200

    Bezig geweest met de horeca app.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Week 9/Opdracht/horecaApp/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Week 9/Opdracht/horecaApp/script.js
@@ -2,7 +2,21 @@
 
 function order(){
 
+    while(drank != 'stop'){
+        var drank = prompt('Welke drank wilt u toevoegen? \n fris of bier of wijn.');
 
+        if(drank == 'fris'){
+            var frisAantal = Number(prompt('Hoeveel fris wilt u bestellen?'));
+        } if(drank == 'bier'){
+            var bierAantal = Number(prompt('Hoeveel bier wilt u bestellen?'));
+        } if(drank == 'wijn'){
+            var wijnAantal = Number(prompt('Hoeveel wijn wilt u bestellen?'));
+        } 
+    }
+
+    document.write('Aantal bestelde frisdrank: ' + frisAantal + "<br>");
+    document.write('Aantal bestelde bier dranken: ' + bierAantal + "<br>");
+    document.write('Aantal bestelde wijn dranken:' + wijnAantal);
 }
 
 order();
