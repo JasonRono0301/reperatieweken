1e99b7676e0fd3a45883798fbf9302fd37e6fe15
Jason <99057964@mydavinci.nl>
2021-04-14 12:07:58 +0200

    Bezig met de adventure game.
    Bezig met het maken van de levels.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
@@ -5,28 +5,45 @@ var description = document.getElementById('description');
 var inventory = document.getElementById('inventoryItem');
 var buttonLayout = document.getElementById('game-buttons');
 
-var buttons = [];
-buttons[0] = document.getElementById('button1');
-buttons[1] = document.getElementById('button2');
-buttons[2] = document.getElementById('button3');
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
 
 var background = document.getElementById('game-container');
 
 var titels = [
-    'Welkom',
-    'Test'
+    'Intro van het verhaal.',
+    'Verlaat het huis',
+    'Zoek de sleutel voor het poort'
 ];
 
 var stories = [
-    'Welkom bij de Adventure game'
+    'Welkom bij de adventure game waarbij jij het gouden wapen moet gaan vinden. En het Goblin rijk moet vernietigen.',
+    'Ga door de deur heen om het huis te verlaten.'
 ];
 
-function testLevel(){
-    title.innerHTML = titels[0];
-    description.innerHTML = stories[0];
+function TheGame(){
+
+    function start(){
+        title.innerHTML = titels[0];
+        description.innerHTML = stories[0];
+        inventory.style.display = 'none';
+            button1.innerHTML = ">>>";
+            button2.onclick = function() {level1()};
+            button3.innerHTML = "<<<";
+    } start();
+    
+    function level1(){
+        title.innerHTML = titels[1];
+        description.innerHTML = stories[1];
+    }
+
+
 }
 
-testLevel();
+TheGame();
+
+
 
 
 
