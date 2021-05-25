5a2b819ae0f46622669561db082c5b4a41789141
Jason <99057964@mydavinci.nl>
2021-05-25 12:01:53 +0200

    Bezig met het maken van een formulier met behulp van array.

---
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Extra opdrachten/Arrays/Array Namen/script.js
@@ -0,0 +1,14 @@
+//Array namen.
+
+var auto = [
+    'Audi',
+    'Bmw',
+    'Opel',
+    'Range rover',
+    'Ferrari'
+];
+
+var autoNaam = document.getElementById('input');
+var knop = document.getElementById('knop');
+var resultaat = document.getElementById('resultaat');
+
