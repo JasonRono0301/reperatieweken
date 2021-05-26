a2465e8974de83900fd9c4d60d067d8ee971185d
Jason <99057964@mydavinci.nl>
2021-05-26 16:37:19 +0200

    Woord raad prototype werk.
    Nog bezig ermee.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Extra opdrachten/Arrays/Array Namen/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Extra opdrachten/Arrays/Array Namen/script.js
@@ -1,14 +1,12 @@
 //Array namen.
 
-var auto = [
-    'Audi',
-    'Bmw',
-    'Opel',
-    'Range rover',
-    'Ferrari'
-];
-
-var autoNaam = document.getElementById('input');
+var naam = document.getElementById('naam');
 var knop = document.getElementById('knop');
 var resultaat = document.getElementById('resultaat');
 
+
+knop.addEventListener('click', function(){
+    var waarde = naam.value;
+    alert(waarde);
+    resultaat.innerHTML = waarde;
+})
