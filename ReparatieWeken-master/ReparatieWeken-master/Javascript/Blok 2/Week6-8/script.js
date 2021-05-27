4c2709685fb2ce524c588c792852d3efea9baf0d
Jason <99057964@mydavinci.nl>
2021-05-27 15:05:00 +0200

    Bezig geweest met de prototype van lingo.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/script.js
@@ -2,23 +2,28 @@ var woord = document.getElementById('naam');
 var check = document.getElementById('check');
 var knop = document.getElementById('knop');
 var uitkomst = "";
-var games = ['Call of Duty','Apex Legends', 'FIFA', 'Battlefield'];
+var games = ['Fiets','Speen','breuk','bomen','draai'];
 
 knop.onclick = function(){
     for(i = 0; i < games.length; i++){
         if(woord.value == games[i]){
             uitkomst = 'Correct';
             i = games.length;
-        } else{
+        } else {
             uitkomst = 'Niet gevonden.';
         }
+        check.innerHTML = "<h1>" + uitkomst;
     }
 
     var splitwoord = woord.value;
     var split = splitwoord.split('');
     console.log(split);
 
-    check.innerHTML = "<h1>" + split;
+    for(i=0; i < games.length; i++){
+        var button = document.createElement('button');
+            check.appendChild(button);
+            button.innerHTML = split[i];
+    }
 }
 
 
