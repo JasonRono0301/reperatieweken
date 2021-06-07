1ee74bc5e53c5ff109011e37545541f2ef8f413b
Jason <99057964@mydavinci.nl>
2021-06-07 13:43:05 +0200

    Begonnen met lab 1 opdracht D.

---
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab1/opdrachtD/script.js
@@ -0,0 +1,24 @@
+var container = document.getElementById('container');
+
+for(i = 1; i <= 30; i++){
+    var button = document.createElement('button');
+        container.appendChild(button);
+        button.innerHTML = i;
+        button.style.backgroundColor = 'green';
+
+    button.onclick = function(){
+        var buttonColor = this.style.backgroundColor;
+        if(buttonColor == 'green'){
+            this.style.backgroundColor = 'red';
+        }
+        if(buttonColor == 'red'){
+            this.style.backgroundColor = 'purple';
+        }
+        if(buttonColor == 'purple'){
+            this.style.backgroundColor = 'black';
+        }
+        if(buttonColor == 'black'){
+            this.style.visibility = 'hidden';
+        }
+    }
+}
