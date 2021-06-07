aa47503f215c67309969e007e8ee91e72ad08c9c
Jason <99057964@mydavinci.nl>
2021-06-07 09:20:45 +0200

    Test gedaan met de buttons die van kleur veranderd getest.
    Het werkt.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab1/opdrachtC/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab1/opdrachtC/script.js
@@ -1,13 +1,30 @@
 var container = document.getElementById('container');
+var count = 0;
 
 for(i=1; i <= 30; i++){
     var buttons = document.createElement('button');
     container.appendChild(buttons);
     buttons.innerHTML = i;
 
-    if(i == 5 || 6){
+    if(i == 5){
         buttons.onclick = function(){
-            alert('Yo');
+            count++;
+            console.log(count);
+            if(count > 0){
+                this.style.backgroundColor = "red";
+                if(count > 1){
+                    this.style.backgroundColor = "purple";
+                    if(count > 2){
+                        this.style.backgroundColor = "blue";
+                        if(count > 3){
+                            this.style.backgroundColor = "black";
+                            if(count > 4){
+                                this.style.visibility = "hidden";
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
