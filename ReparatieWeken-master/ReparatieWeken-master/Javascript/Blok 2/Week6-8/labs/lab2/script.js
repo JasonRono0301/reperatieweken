bea17eb1008cd45edc674f900e248f92fc8774cc
Jason <99057964@mydavinci.nl>
2021-06-08 10:55:39 +0200

    Lab 2 af.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab2/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab2/script.js
@@ -5,8 +5,17 @@ function arrayNames(){
     var vraag = prompt('Hoeveel namen wilt u in de array stoppen? (minimaal 3)');
     if(vraag < 3){
         alert('U moet minimaal 3 namen invoeren.');
-    } if(vraag > 3){
-    
+    } if(vraag >= 3){
+        for(i = 0; i < vraag; i++){
+            var antwoord = prompt('Type hier de namen in:');
+            namen.push(antwoord);
+            document.write(namen[i] + "<br>");
+        }
+
+        for(i = namen.length-1; i>= 0; i--){
+            document.write(namen[i] + "<br>");
+        }
+        console.log(namen);
     }
 
 
