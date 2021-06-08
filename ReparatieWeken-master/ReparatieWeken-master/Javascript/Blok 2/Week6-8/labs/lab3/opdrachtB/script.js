a018091bee469928a8e1bb663dbfd9938c8d2790
Jason <99057964@mydavinci.nl>
2021-06-08 14:30:50 +0200

    Bezig geweest met lab 3 opdracht B.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab3/opdrachtB/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab3/opdrachtB/script.js
@@ -1,15 +1,19 @@
+var knop = document.getElementById('knop');
 var getallen = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-var button = document.getElementById('knop');
-var input = document.getElementById('inputVeld');
+knop.onclick = pyramide;
 
-button.onclick = piramide;
+function pyramide(){
+   var inputVeld = document.getElementById('inputVeld');
+   var inputValue = inputVeld.value;
 
-function piramide(){
-   var inputValue = input.value;
-
-   for(i = inputValue;  i <= getallen.length; i++){
-        
-   }
+   for(i = getallen.length; i >= 0; i--){
+      for(j = inputValue; j < i; j++){
+          console.log(j);
+          document.write(j);
+      }
+      console.log("\n");
+      document.write("<br>");
+  }
 }
  
