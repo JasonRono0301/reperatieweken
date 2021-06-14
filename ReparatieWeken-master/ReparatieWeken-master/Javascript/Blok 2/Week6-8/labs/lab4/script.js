00192b7b90aa9237e6266527938e2d00d96a7ef5
Jason <99057964@mydavinci.nl>
2021-06-14 10:48:56 +0200

    Bezig met lab 4.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab4/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab4/script.js
@@ -2,21 +2,14 @@ var woord = document.getElementById('naam');
 var check = document.getElementById('check');
 var knop = document.getElementById('knop');
 var uitkomst = "";
-var games = ['Fiets','Speen','breuk','bomen','draai'];
+var games = ['Fiets'];
 
 knop.onclick = split;
 
-function split(){
+function split(){ 
     var woordValue = woord.value;
     var split = woordValue.split('');
-    console.log(split);
-
-    for(i = 0; i < games.length; i++){
-        var letters = document.createElement('button');
-        check.appendChild(letters);
-        letters.innerHTML = split[i];
-    }    
-
+    console.log(split); 
 }
 
 
