04cf6f2ccb6b5c823f12d307d1ae5cc43a4255a9
Jason <99057964@mydavinci.nl>
2021-06-14 14:06:10 +0200

    Heb het juiste woord uit de array kunnen splitten.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab4/script.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week6-8/labs/lab4/script.js
@@ -2,14 +2,18 @@ var woord = document.getElementById('naam');
 var check = document.getElementById('check');
 var knop = document.getElementById('knop');
 var uitkomst = "";
-var games = ['Fiets'];
+var games = ['fiets'];
 
 knop.onclick = split;
 
 function split(){ 
+    var correctWord = games[0];
+    var correctWordSplit = correctWord.split('');
+    console.log(correctWordSplit);
+
     var woordValue = woord.value;
-    var split = woordValue.split('');
-    console.log(split); 
+    var inputSplit = woordValue.split('');
+    console.log(inputSplit); 
 }
 
 
