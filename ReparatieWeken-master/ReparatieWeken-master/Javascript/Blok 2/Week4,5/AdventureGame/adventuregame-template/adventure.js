020088133fbd89b216aa8ff48b17c8cb954d5f96
Jason <99057964@mydavinci.nl>
2021-09-16 11:29:49 +0200

    De opdracht opnieuw aan het maken.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
@@ -11,37 +11,7 @@ var button3 = document.getElementById('button3');
 
 var background = document.getElementById('game-container');
 
-var titels = [
-    'Intro van het verhaal.',
-    'Verlaat het huis',
-    'Zoek de sleutel voor het poort'
-];
 
-var stories = [
-    'Welkom bij de adventure game waarbij jij het gouden wapen moet gaan vinden. En het Goblin rijk moet vernietigen.',
-    'Ga door de deur heen om het huis te verlaten.'
-];
-
-function TheGame(){
-
-    function start(){
-        title.innerHTML = titels[0];
-        description.innerHTML = stories[0];
-        inventory.style.display = 'none';
-            button1.innerHTML = ">>>";
-            button2.onclick = function() {level1()};
-            button3.innerHTML = "<<<";
-    } start();
-    
-    function level1(){
-        title.innerHTML = titels[1];
-        description.innerHTML = stories[1];
-    }
-
-
-}
-
-TheGame();
 
 
 
