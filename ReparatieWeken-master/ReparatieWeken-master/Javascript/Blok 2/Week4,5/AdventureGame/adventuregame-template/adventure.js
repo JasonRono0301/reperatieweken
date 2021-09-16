f8b8ed3e0728800940cc42df27215644002ebbb7
Jason <99057964@mydavinci.nl>
2021-09-16 13:38:20 +0200

    Bezig met de adventure game.

--- ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
+++ ReparatieWeken-master/ReparatieWeken-master/Javascript/Blok 2/Week4,5/AdventureGame/adventuregame-template/adventure.js
@@ -11,7 +11,39 @@ var button3 = document.getElementById('button3');
 
 var background = document.getElementById('game-container');
 
+function TheGame(){
+    var titles = [
+        'Intro',
+        'Level 1'
 
+    ];
+    var descriptions = [
+        'Druk op de knop',
+        'Open de deur'
+    ];
+
+    function intro(){
+        title.innerHTML = titles[0];
+        description.innerHTML = descriptions[0];
+        button1.innerHTML = 'Start';
+        button1.addEventListener('click',level1);
+        button2.style.visibility = 'hidden';
+        button3.style.visibility = 'hidden';
+        inventory.style.visibility = 'hidden';
+
+    }
+    intro();
+
+    function level1(){
+        title.innerHTML = titles[1];
+        description.innerHTML = descriptions[1];
+        button1.addEventListener('click', intro);
+        button1.innerHTML = 'Open';
+    }
+
+}
+
+TheGame();
 
 
 
